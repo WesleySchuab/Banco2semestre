387c61ce82802618340e817fbc63eb10da8a7f75
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-08-26 20:17:33 -0300

    Criando a tabela

--- Codigos/Scrip1.sql
+++ Codigos/Scrip1.sql
@@ -5,3 +5,11 @@ GO
 --Habilitar o contexto 
 USE Empresacj3024865;
 GO
+-- Criando a tabela
+CREATE TABLE FUNCIONARIOS (
+	ID INT PRIMARY KEY,
+	Nome VARCHAR(25) NOT NULL,
+	Sexo CHAR(1) NULL,
+	Admissao DATE NOT NUll,
+	Salario Decimal(10,2) NOT NULL
+);
