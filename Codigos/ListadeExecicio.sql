69bd08f3c53f382181b151f17aa858c8b874eb2e
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-09-16 22:13:57 -0300

    Otendo informações sobre o banco

--- Codigos/ListadeExecicio.sql
+++ Codigos/ListadeExecicio.sql
@@ -88,25 +88,53 @@ GO
 
 -- Insere os registros corrigindo os erros
 INSERT INTO CLIENTES VALUES
-    (2, 'Marcos Paulo', 'M', 45, '22222222222', 
+    (4, 'Marcos Paulo', 'M', 45, '22222222222', 
 'marcos@gmail.com'),
-    (3, 'Andre Luis', 'M', 25, '333333333333',
+    (3, 'Andre Luis', 'M', 25, '33333333333',
 'andre@gmail.com'),
-    (5, 'Pedro Augusto', 'M', 45, '11111111111',
+    (5, 'Pedro Augusto', 'M', 45, '12121111111',
 'pedro@gmail.com'),
     (6, 'Ricardo Lima', 'M', 52, '66666666666',
 'ricardo@gmail.com'),
     (7, 'Jose Pereira ', 'M', 45, '77777777777',
 'marcos@gmail.com'),
-    (8, 'Marcelo Souza', 'Masculino', 56, '87777777777',
+    (8, 'Marcelo Souza', 'M', 56, '87777777777',
 'marcelo@gmail.com'),
-    (10, 'Sheila Pereira', NULL, 21, 10101010101),
+    (10, 'Sheila Pereira', NULL, 21, '10101010101',
 'sheila@yahoo.com.br'),
     (11,'Tiago Augusto', NULL, 70, '20202020202',
 'tiago@yahoo.com.br'),
     (12, 'Maria Pereira', 'F', 45, '30303030303',
     'maria@bol.com.br');
 GO 
+
+-- Inseri os registro sem email
+INSERT INTO CLIENTES (ID, Nome, Sexo, Idade, CPF )
+    VALUES (13, 'Lucas Silva', 'M', 19, '40404040404'),
+            (14, 'Benedito SIlva', NULL, 44, '505050505'),
+            (15, 'Fernanda Pereira', 'F', 31, '606060606');
+GO           
+
+-- Exibi Infomarações sobre a estrutura da tabela
+SELECT TABLE_CATALOG    AS 'Banco de Dados',
+        TABLE_NAME      AS 'Tabela',
+        ORDINAL_POSITION AS 'Posição',
+        COLUMN_NAME AS 'Coluna',
+        DATA_TYPE AS 'Tipo de Dados',
+        COLLATION_NAME AS 'Idioma da Coluna',
+        IS_NULLABLE AS ' Aceita Nulo ?'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Clientes';
+GO
+
+-- Exibi as informações sobre a sRestrições
+SELECT CONSTRAINT_CATALOG AS 'Banco de Dados',
+         TABLE_NAME      AS 'Tabela',
+         CONSTRAINT_TYPE AS 'Tipo de restrições',
+         CONSTRAINT_NAME AS 'Nome da Restrição'
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE TABLE_NAME = 'Clientes';
+GO
 -- Verifica a estrutura da tabela usando o comando
 -- sp_help
 EXEC sp_help 'CLIENTES';
