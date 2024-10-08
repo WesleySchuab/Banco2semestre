c913c634e0d206c759196e0458b20a29e1a30db4
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-10-07 22:07:15 -0300

    Segundo execicio da Lista

--- Codigos/Lista_03.sql
+++ Codigos/Lista_03.sql
@@ -21,3 +21,21 @@ ALTER TABLE CLIENTES
      Telefone CHAR(10);
 GO
 
+UPDATE CLIENTES SET Nome = 'Ana Cristina', DDD = '12', Telefone = '3664-5060' WHERE ID = 1;
+UPDATE CLIENTES SET Nome = 'Marcos Paulo', DDD = '12', Telefone = '3664-8090' WHERE ID = 2;
+UPDATE CLIENTES SET Nome = 'André Luís', DDD = '12', Telefone = '3663-1070' WHERE ID = 3;
+UPDATE CLIENTES SET Nome = 'Maria Clara', DDD = NULL , Telefone = NULL WHERE ID = 4;
+UPDATE CLIENTES SET Nome = ' Pedro Augusto', DDD = '12', Telefone = '3667-6070' WHERE ID = 5;
+UPDATE CLIENTES SET Nome = ' Ricardo Lima', DDD = NULL , Telefone = NULL WHERE ID = 6;
+UPDATE CLIENTES SET Nome = ' José Pereira', DDD = '12', Telefone = '98790-6070' WHERE ID = 7;
+UPDATE CLIENTES SET Nome = ' Marcelo Souza ', DDD = NULL , Telefone = NULL WHERE ID = 8;
+UPDATE CLIENTES SET Nome = ' Daphne Lima', DDD = '12', Telefone = '97070-6070' WHERE ID = 9;
+UPDATE CLIENTES SET Nome = ' Sheila Pereira ', DDD = NULL , Telefone = NULL WHERE ID = 10;
+UPDATE CLIENTES SET Nome = ' Tiago Augusto ', DDD = NULL , Telefone = NULL WHERE ID = 11;
+UPDATE CLIENTES SET Nome = ' Maria Pereira', DDD = '11', Telefone = '99555-001' WHERE ID = 12;
+UPDATE CLIENTES SET Nome = ' Lucas silva', DDD = '11', Telefone = '97890-1010' WHERE ID = 13;
+UPDATE CLIENTES SET Nome = ' Benedito Silva ', DDD = NULL , Telefone = NULL WHERE ID = 14;
+UPDATE CLIENTES SET Nome = ' Fernada Pereira ', DDD = NULL , Telefone = '98888-0102' WHERE ID = 15;
+
+
+
