9a7699c7e8c80ca1a8398c0a0d7161033fcd9b1d
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-10-20 11:24:50 -0300

    criando tabela com restrição e verificação

--- Codigos/Lista2.sql
+++ Codigos/Lista2.sql
@@ -8,3 +8,14 @@ GO
 SELECT create_date AS 'Data de Criação',
     name AS 'Nome da Tabela'
 FROM Sys.tables 
+
+-- Cria tabela para armazenar os clientes
+CREATE TABLE CLIENTES (
+    ID INT PRIMARY KEY ,
+    Nome  VARCHAR ( 50 ) NOT NULL,
+    Sexo CHAR ( 1 ) NULL,
+    Idade INT CHECK ( Idade > 18 ) NOT NULL,
+    CPF CHAR ( 11 ) UNIQUE NOT NULL ,
+    Email VARCHAR ( 200 ) DEFAULT 'meu@email.com' NOT NULL
+);
+GO
