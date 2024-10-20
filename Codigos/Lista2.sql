9ed360e1edb24a931a42bfaa509ef6bf6bb4ddf9
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-10-20 11:34:52 -0300

    testando as restrições

--- Codigos/Lista2.sql
+++ Codigos/Lista2.sql
@@ -19,3 +19,20 @@ CREATE TABLE CLIENTES (
     Email VARCHAR ( 200 ) DEFAULT 'meu@email.com' NOT NULL
 );
 GO
+
+-- Verifica a inserção dos registros com restrições
+-- OK, registros inseridos normalmente 
+INSERT INTO CLIENTES VALUES
+    (1, 'Ana Cristina', 'F', 20, '11111111111',
+'ana@gmail.com');
+GO
+
+-- Erro devido ao valor repetido da chave primaria
+INSERT INTO CLIENTES VALUES 
+(1, 'Marcos Paulo', 'M', 45, '22222222222',
+'marcos@gmail.com');
+GO
+
+-- Erro no azure
+Msg 2627, Level 14, State 1, Line 1
+Violation of PRIMARY KEY constraint 'PK__CLIENTES__3214EC277C3456C8'. Cannot insert duplicate key in object 'dbo.CLIENTES'. The duplicate key value is (1).
